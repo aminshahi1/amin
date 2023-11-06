c374fcbd49b3af04e32c3c79c6b29ab99fd4620c
aminshahi1 <it.aminshahi@gmail.com>
2023-11-07 00:06:16 +0100

    u

--- routes/router.js
+++ routes/router.js
@@ -1,66 +1,95 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult} = require('express-validator')
-let v = 0;
-let b = 0;
-let c = 0;
 
+const { check, validationResult } = require('express-validator');
+let User1 = require('./../models/Users');
+let User;
+let login = 0;
+let signup = 0;
 
-
-let users = require('./../Users');
-users.sort();
-
-router.get('/', (req,res)=>{
-    users.sort();
-    res.render('index', {users : users , v,b,c});
-    v = 0;
+router.get('/', async (req,res)=>{
+    User = await User1.find({});
+    login = 0;
+    signup = 0;
+    res.render('Home', {users : User, signup, login});
+});
+router.get('/Home', async (req,res)=>{
+    User = await User1.find({});
+    login = 0;
+    signup = 0;
+    res.render('Home', {users : User, signup, login});
+});
+router.get('/About', (req,res)=>{
+    login = 0;
+    signup = 0;
+    res.render('About', {users : User, signup, login});
+})
+router.get('/Login', async (req,res)=>{
+    User = await User1.find({});
+    res.render('Login', {users : User, signup, login});
 })
 
-router.post('/',
-    check('id').isInt(),
-    check('name').isString(),
+
+
+router.post('/signup',
     check('email').isEmail(),
-    (req,res)=>{
-    users.sort();
+    check('password').isLength({min : 5}),
+    async (req,res)=>{
+    User = await User1.find({});
     const errors = validationResult(req);
-    if(errors.isEmpty()) {
-        for (let a = 0; a < 1; a++) {
-            req.body.id = parseInt(req.body.id);
-            users[users.length] = {id: req.body.id, name: req.body.name, email: req.body.email};
-            v = 0;
-            b = 1;
-            c = 0;
-        }
-    }else if(!errors.isEmpty()){
-        v = v+1;
-        b = 0;
-        c = 0;
+    if(!errors.isEmpty()){
+        signup = 1;
     }
-        res.render('index', {users : users, v,b,c});
-})
-
-router.delete('/:id', (req,res)=>{
-    users.sort();
-    users = users.filter(user => {
-        if(user.id != req.params.id){
-            c = 1;
-            v = 0;
-            b = 0;
-            return user;
+    if(errors.isEmpty()){
+        signup = 0;
+    }
+    for(let c = 0; c < User.length; c++){
+        if(req.body.email == User[c].email){
+            signup = 1;
         }
-    })
-    res.render('index', {users : users , v, b,c});
-})
+    }
+    if (signup == 1){
+        res.render('Home', {users : User, signup, login});
+    }
+    if (signup == 0){
+        req.body.age = parseInt(req.body.age);
+        const user = new User1({
+            id: User.length+1,
+            first_name: req.body.fname,
+            last_name: req.body.lname,
+            age: req.body.age,
+            email: req.body.email,
+            password: req.body.password
+        });
 
-router.get('/user/:id', (req,res)=>{
-    var w;
-    for(let a = 0; a <users.length; a++){
-        if(users[a].id = req.params.id){
+        await user.save();
+
+        res.render('Login', {users : User, signup, login});
+    }
+});
+
+
+
+
+
+router.post('/login', async (req,res)=>{
+    User = await User1.find({});
+    login = 0;
+    let w ;
+    let help = 1;
+    for(let a = 0; a < User.length; a++){
+        if(User[a].email == req.body.email2 && User[a].password == req.body.password2){
+            login = 0;
             w = a;
+            help = 0;
+            res.render('Wellcome', {users : User, signup, login, w});
         }
     }
-    res.render('user',{users : users , w});
+
+    if(help == 1){
+        login = 1;
+        res.render('Login', {users:User ,login,signup});
+    }
+
 })
-
-
 module.exports = router;
